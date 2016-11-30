6f13feddad8a263702fc254fbd0e0db6631dbe7f
Joseph C Wang <joequant@gmail.com>
2016-12-01 00:37:54 +0800

    add machine driver

--- linux-4.8/cht_bsw_nau8824.c
+++ linux-4.8/cht_bsw_nau8824.c
@@ -1,10 +1,10 @@
 /*
- *  cht-bsw-max98090.c - ASoc Machine driver for Intel Cherryview-based
- *  platforms Cherrytrail and Braswell, with max98090 & TI codec.
+ *  cht-bsw-nau8824.c - ASoc Machine driver for Intel Cherryview-based
+ *  platforms Cherrytrail and Braswell, with nau8824 codec.
  *
- *  Copyright (C) 2015 Intel Corp
- *  Author: Fang, Yang A <yang.a.fang@intel.com>
- *  This file is modified from cht_bsw_rt5645.c
+ *  Copyright (C) 2015 Bitquant Research Laboratories (Asia) Limited
+ *  Author: Wang, Joseph C <joequant@gmail.com>
+ *  This file is modified from cht_bsw_max98090_ti.c
  *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -27,16 +27,14 @@
 #include <sound/pcm_params.h>
 #include <sound/soc.h>
 #include <sound/jack.h>
-#include "../../codecs/max98090.h"
-#include "../atom/sst-atom-controls.h"
-#include "../../codecs/ts3a227e.h"
+#include "sst-atom-controls.h"
+#include "nau8824.h"
 
 #define CHT_PLAT_CLK_3_HZ	19200000
 #define CHT_CODEC_DAI	"HiFi"
 
 struct cht_mc_private {
 	struct snd_soc_jack jack;
-	bool ts3a227e_present;
 };
 
 static inline struct snd_soc_dai *cht_get_codec_dai(struct snd_soc_card *card)
@@ -84,42 +82,9 @@ static const struct snd_kcontrol_new cht_mc_controls[] = {
 static int cht_aif1_hw_params(struct snd_pcm_substream *substream,
 			     struct snd_pcm_hw_params *params)
 {
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_dai *codec_dai = rtd->codec_dai;
 	int ret;
-
-	ret = snd_soc_dai_set_sysclk(codec_dai, M98090_REG_SYSTEM_CLOCK,
-				     CHT_PLAT_CLK_3_HZ, SND_SOC_CLOCK_IN);
-	if (ret < 0) {
-		dev_err(rtd->dev, "can't set codec sysclk: %d\n", ret);
-		return ret;
-	}
-
 	return 0;
 }
-
-static int cht_ti_jack_event(struct notifier_block *nb,
-		unsigned long event, void *data)
-{
-	struct snd_soc_jack *jack = (struct snd_soc_jack *)data;
-	struct snd_soc_dapm_context *dapm = &jack->card->dapm;
-
-	if (event & SND_JACK_MICROPHONE) {
-		snd_soc_dapm_force_enable_pin(dapm, "SHDN");
-		snd_soc_dapm_force_enable_pin(dapm, "MICBIAS");
-		snd_soc_dapm_sync(dapm);
-	} else {
-		snd_soc_dapm_disable_pin(dapm, "MICBIAS");
-		snd_soc_dapm_disable_pin(dapm, "SHDN");
-		snd_soc_dapm_sync(dapm);
-	}
-
-	return 0;
-}
-
-static struct notifier_block cht_jack_nb = {
-	.notifier_call = cht_ti_jack_event,
-};
 
 static int cht_codec_init(struct snd_soc_pcm_runtime *runtime)
 {
@@ -135,12 +100,7 @@ static int cht_codec_init(struct snd_soc_pcm_runtime *runtime)
 	* KEY_VOLUMEUP
 	* KEY_VOLUMEDOWN
 	*/
-	if (ctx->ts3a227e_present)
-		jack_type = SND_JACK_HEADPHONE | SND_JACK_MICROPHONE |
-					SND_JACK_BTN_0 | SND_JACK_BTN_1 |
-					SND_JACK_BTN_2 | SND_JACK_BTN_3;
-	else
-		jack_type = SND_JACK_HEADPHONE | SND_JACK_MICROPHONE;
+	jack_type = SND_JACK_HEADPHONE | SND_JACK_MICROPHONE;
 
 	ret = snd_soc_card_jack_new(runtime->card, "Headset Jack",
 					jack_type, jack, NULL, 0);
@@ -149,10 +109,6 @@ static int cht_codec_init(struct snd_soc_pcm_runtime *runtime)
 		dev_err(runtime->dev, "Headset Jack creation failed %d\n", ret);
 		return ret;
 	}
-
-	if (ctx->ts3a227e_present)
-		snd_soc_jack_notifier_register(jack, &cht_jack_nb);
-
 	return ret;
 }
 
@@ -196,26 +152,12 @@ static int cht_aif1_startup(struct snd_pcm_substream *substream)
 			SNDRV_PCM_HW_PARAM_RATE, 48000);
 }
 
-static int cht_max98090_headset_init(struct snd_soc_component *component)
-{
-	struct snd_soc_card *card = component->card;
-	struct cht_mc_private *ctx = snd_soc_card_get_drvdata(card);
-
-	return ts3a227e_enable_jack_detect(component, &ctx->jack);
-}
-
 static struct snd_soc_ops cht_aif1_ops = {
 	.startup = cht_aif1_startup,
 };
 
 static struct snd_soc_ops cht_be_ssp2_ops = {
 	.hw_params = cht_aif1_hw_params,
-};
-
-static struct snd_soc_aux_dev cht_max98090_headset_dev = {
-	.name = "Headset Chip",
-	.init = cht_max98090_headset_init,
-	.codec_name = "i2c-104C227E:00",
 };
 
 static struct snd_soc_dai_link cht_dailink[] = {
@@ -260,7 +202,7 @@ static struct snd_soc_dai_link cht_dailink[] = {
 		.platform_name = "sst-mfld-platform",
 		.no_pcm = 1,
 		.codec_dai_name = "HiFi",
-		.codec_name = "i2c-193C9890:00",
+		.codec_name = "i2c-10508824:00",
 		.dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF
 					| SND_SOC_DAIFMT_CBS_CFS,
 		.init = cht_codec_init,
@@ -273,12 +215,10 @@ static struct snd_soc_dai_link cht_dailink[] = {
 
 /* SoC card */
 static struct snd_soc_card snd_soc_card_cht = {
-	.name = "chtmax98090",
+	.name = "chtnau8824",
 	.owner = THIS_MODULE,
 	.dai_link = cht_dailink,
 	.num_links = ARRAY_SIZE(cht_dailink),
-	.aux_dev = &cht_max98090_headset_dev,
-	.num_aux_devs = 1,
 	.dapm_widgets = cht_dapm_widgets,
 	.num_dapm_widgets = ARRAY_SIZE(cht_dapm_widgets),
 	.dapm_routes = cht_audio_map,
@@ -296,13 +236,6 @@ static int snd_cht_mc_probe(struct platform_device *pdev)
 	if (!drv)
 		return -ENOMEM;
 
-	drv->ts3a227e_present = acpi_dev_found("104C227E");
-	if (!drv->ts3a227e_present) {
-		/* no need probe TI jack detection chip */
-		snd_soc_card_cht.aux_dev = NULL;
-		snd_soc_card_cht.num_aux_devs = 0;
-	}
-
 	/* register the soc card */
 	snd_soc_card_cht.dev = &pdev->dev;
 	snd_soc_card_set_drvdata(&snd_soc_card_cht, drv);
@@ -318,7 +251,7 @@ static int snd_cht_mc_probe(struct platform_device *pdev)
 
 static struct platform_driver snd_cht_mc_driver = {
 	.driver = {
-		.name = "cht-bsw-max98090",
+		.name = "cht-bsw-nau8824",
 	},
 	.probe = snd_cht_mc_probe,
 };
@@ -326,6 +259,6 @@ static struct platform_driver snd_cht_mc_driver = {
 module_platform_driver(snd_cht_mc_driver)
 
 MODULE_DESCRIPTION("ASoC Intel(R) Braswell Machine driver");
-MODULE_AUTHOR("Fang, Yang A <yang.a.fang@intel.com>");
+MODULE_AUTHOR("Wang, Joseph C <joequant@gmail.com>");
 MODULE_LICENSE("GPL v2");
-MODULE_ALIAS("platform:cht-bsw-max98090");
+MODULE_ALIAS("platform:cht-bsw-nau8824");
