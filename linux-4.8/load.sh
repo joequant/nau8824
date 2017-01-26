78dd1df976ab634d7a81e3fc44b0e54974276b0a
Joseph C Wang <joequant@gmail.com>
2017-01-26 16:54:49 +0800

    fix load

--- linux-4.8/load.sh
+++ linux-4.8/load.sh
@@ -1,4 +1,4 @@
-insmod snd-soc-nau8824.ko
-insmod snd-soc-sst-cht-bsw-nau8824.ko
+insmod snd-soc-nau8824.ko.xz
+insmod snd-soc-sst-cht-bsw-nau8824.ko.xz
 rmmod snd_intel_sst_acpi
-insmod snd-intel-sst-acpi.ko
+insmod snd-intel-sst-acpi.ko.xz
